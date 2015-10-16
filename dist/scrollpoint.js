7a6eb1778f43cfc12559693e4e82d3badb02d0a8
Daniel Karp <danielkarp@gmail.com>
2015-10-16 10:31:12 -0400

    Added a demo for resetting scrollpoints

--- dist/scrollpoint.js
+++ dist/scrollpoint.js
@@ -1,7 +1,7 @@
 /*!
  * angular-ui-scrollpoint
  * https://github.com/angular-ui/ui-scrollpoint
- * Version: 1.0.1 - 2015-10-13T16:34:19.786Z
+ * Version: 1.0.1 - 2015-10-16T14:30:35.778Z
  * License: MIT
  */
 
